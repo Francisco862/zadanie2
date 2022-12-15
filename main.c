9f70203b430d03cc17ffe6f42e6b765b81159ba4
Francisco862 <renoverxdd@gmail.com>
2022-12-16 00:39:35 +0100

    ukonczono program

--- main.c
+++ main.c
@@ -8,6 +8,13 @@ int main() {
 			scanf("%d", &macierz[x][z]);
 		}
 	}
+	printf("Macierz z podanych liczb \n");
+	for (x = 0; x < 3; x++) {
+		for (z = 0; z < 3; z++) {
+			printf("%d ", macierz[x][z]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
 
